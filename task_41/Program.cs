9024d81895df8ba16647799a5c72348b1cdbd3ce
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-22 16:08:53 +0700

    41

---
+++ task_41/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите числа через пробел: ");
+double[] size = Console.ReadLine().
+Split(" ").Select(x => double.Parse(x)).ToArray();
+double count = 0;
+for (int i = 0; i < size.Length; i++)
+{ if (size[i] > 0) count = count + 1; }
+Console.WriteLine($"Количество чисел больше 0: {count}.");
